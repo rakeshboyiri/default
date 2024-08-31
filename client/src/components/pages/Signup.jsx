ca9a7af4ff9d20f124a14dc437543df4b3642216
suvam <suvampravu@gmail.com>
2024-09-01 00:34:30 +0530

    complete signup page ui and functionality

--- client/src/components/pages/Signup.jsx
+++ client/src/components/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import Spin from "../utilities/Spin";
+import Spin from "../utility/Spin";
 
 function Signup() {
   const {
